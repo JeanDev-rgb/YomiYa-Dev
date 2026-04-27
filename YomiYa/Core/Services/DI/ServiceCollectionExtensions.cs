60d8ae97ccaeb7255197b14ecb1c9448a570fb68
jeandev09 <jean132309@gmail.com>
2026-04-27 11:05:20 -0500

    Integrar DI con Microsoft.Extensions.DependencyInjection
    Se añade el paquete de DI, se crea ServiceCollectionExtensions y se registra MainWindowViewModel como servicio. Ahora la instancia del ViewModel se obtiene desde el contenedor en App.axaml.cs.

---
+++ YomiYa/Core/Services/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YomiYa.Features.Main;
+
+namespace YomiYa.Core.Services.DI;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCommonServices(this IServiceCollection collection)
+    {
+        collection.AddTransient<MainWindowViewModel>();
+    }
+}
